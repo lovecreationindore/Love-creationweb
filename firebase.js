c89026370873a15716381047745e714700d12c69
Love creation <lovecreationinfo@gmail.com>
2026-01-09 23:52:12 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,18 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_REAL_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyC2hwvU8GEZP_xT9KImqeSlVEGx4Fr1HHc",
+  authDomain: "love-creation-393b7.firebaseapp.com",
+  projectId: "love-creation-393b7",
+  storageBucket: "love-creation-393b7.firebasestorage.app",
+  messagingSenderId: "1090373514764",
+  appId: "1:1090373514764:web:4926c89ee07e1326c2a029"
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
